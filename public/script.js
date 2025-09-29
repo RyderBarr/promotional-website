baf6df132a78d08d68973043045648495324bfec
ryder <tootwo2to22@gmail.com>
2025-09-29 13:26:06 -0700

    arthzatr

--- public/script.js
+++ public/script.js
@@ -1,38 +1,12 @@
-import fs from 'fs/promises'
 
-async function refresh()
-{
 
-        const rawData = await fs.readFile(file,"utf-8")
-        $('members').html('')
-        rawData.forEach(member => {
-                
-                $('members').append(`
-                  
-                        <section class="flex flex-row justify-evenly items-center w-[100vw] h-min-[15vw] m-5">
+// async function refresh()
+// {
 
-                                <div id="text1" class="w-[50vw] h-min-[15vw] flex flex-col justify-around items-center">
+// }
 
-                                        <div class=" w-[15vw]">
+// setInterval(()=>{
 
-                                                ${member.name} is a member
-                                        
-                                                this member is signed up for a news letter${member.newsLetter} 
-                                        
-                                        </div>
+//         refresh()
 
-                                </div>
-
-                        </section>
-                        
-                `)
-
-        });
-
-}
-
-setInterval(()=>{
-
-        refresh()
-
-},1000)
+// },1000)
